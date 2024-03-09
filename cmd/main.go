c8c3d91e0c6285545775c016574f111034504c63
camilo_marin <camarinb20@gmail.com>
2024-03-08 20:55:30 -0500

    Add http adapter and compose for db postgres, setting gin init from .env file

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,40 @@
 package main
 
-import "camarinb2096/form_example/internal/app/config/db"
+import (
+	"camarinb2096/form_example/internal/app/config/db"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Println("Failed to load .env file: ", err)
+	}
 	config := db.NewConfig()
 	_ = db.NewDb(config)
+
+	r := gin.Default()
+
+	log.Println("Gin mode: ", os.Getenv("GIN_MODE"))
+
+	switch os.Getenv("GIN_MODE") {
+	case "release":
+		gin.SetMode(gin.ReleaseMode)
+	case "debug":
+		gin.SetMode(gin.DebugMode)
+	default:
+		gin.SetMode(gin.TestMode)
+	}
+
+	r.POST("/pqrs", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 
 }
--- internal/app/config/db/config.go
+++ internal/app/config/db/config.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,12 +13,19 @@ type Config struct {
 	Host     string `envconfig:"DB_HOST" default:"localhost"`
 	Port     string `envconfig:"DB_PORT" default:"5432"`
 	User     string `envconfig:"DB_USER" default:"postgres"`
-	Password string `envconfig:"DB_PASSWORD" default:"password"`
-	Database string `envconfig:"DB_DATABASE" default:"postgres"`
+	Password string `envconfig:"DB_PASSWORD" default:"postgres"`
+	Database string `envconfig:"DB_DATABASE" default:"mydb"`
 }
 
 func NewConfig() *Config {
-	return &Config{}
+
+	return &Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Database: os.Getenv("DB_DATABASE"),
+	}
 }
 
 func NewDb(cfg *Config) *gorm.DB {
@@ -26,6 +34,7 @@ func NewDb(cfg *Config) *gorm.DB {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Connected to database:", cfg.Database)
 	return db
 
 }
